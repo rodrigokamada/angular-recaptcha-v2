73e74ee1e3b3056a52575ed84db6f985d0895330
Rodrigo Kamada <rodrigo@kamada.com.br>
2025-12-14 23:06:54 -0300

    Renaming the App class name.

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,9 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
+import { App } from './app';
 
-describe('AppComponent', () => {
+describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -11,13 +11,13 @@ describe('AppComponent', () => {
         FormsModule,
       ],
       declarations: [
-        AppComponent,
+        App,
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });

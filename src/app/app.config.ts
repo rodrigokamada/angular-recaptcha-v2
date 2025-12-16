5dc3f3758a625c9a55629733d0778f0f6eb59d1a
Rodrigo Kamada <rodrigo@kamada.com.br>
2025-12-16 14:30:22 -0300

    Updating for Angular 21 support.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,35 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RECAPTCHA_SETTINGS, RecaptchaFormsModule, RecaptchaModule, RecaptchaSettings } from 'ng-recaptcha';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { RECAPTCHA_SETTINGS, RecaptchaSettings } from 'ng-recaptcha';
 
 import { environment } from '../environments/environment';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { routes } from './app.routes';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    RecaptchaFormsModule,
-    RecaptchaModule,
-    AppRoutingModule,
-  ],
+export const appConfig: ApplicationConfig = {
   providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
     {
       provide: RECAPTCHA_SETTINGS,
       useValue: {
         siteKey: environment.recaptcha.siteKey,
       } as RecaptchaSettings,
     },
-  ],
-  bootstrap: [
-    AppComponent,
-  ],
-})
-export class AppModule {
-}
+  ]
+};

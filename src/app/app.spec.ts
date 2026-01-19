965ff43e9e74cc1abbd071d9cb037da31e3d49aa
Rodrigo Kamada <rodrigo@kamada.com.br>
2026-01-19 10:50:28 -0300

    Removing the FormsModule and RouterTestingModule imports.

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,16 +1,10 @@
 import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { FormsModule } from '@angular/forms';
 import { App } from './app';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
-        FormsModule,
-      ],
-      declarations: [
         App,
       ],
     }).compileComponents();
